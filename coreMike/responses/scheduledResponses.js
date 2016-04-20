8cea61ce62e12455fb815040148846aefe1b0b11
Kevin Roberts <kroberts@mathworks.com>
2016-04-20 11:14:07 -0500

    updates to scheduling

--- coreMike/responses/scheduledResponses.js
+++ coreMike/responses/scheduledResponses.js
@@ -1,24 +1,24 @@
 var CronJob = require('cron').CronJob;
 
 var dayOfTheWeekResponses = require('./dayOfTheWeek');
+var development = process.env.NODE_ENV !== 'production';
 
 var scheduledResponses = function(bot) {
 
     var job = new CronJob({
-        cronTime: '00 10 10 * * 1-5',
+        cronTime: '00 10 09 * * 1-5',
         onTick: function() {
             /*
              * Runs every weekday (Monday through Friday)
-             * at 10:10:00 AM. It does not run on Saturday
+             * at 9:10:00 AM. It does not run on Saturday
              * or Sunday.
              */
-            bot.api.chat.postMessage({'channel' : 'general', 'text' : 'Mornin Mornin!', 'as_user' : true}, function (err, res) {
-                console.log(res);
+            bot.api.chat.postMessage({'channel' : 'general', 'text' : 'Mornin Mornin!\n' + dayOfTheWeekResponses.statementResponse(), 'as_user' : true}, function (err, res) {
+                if (development) {
+                    console.log(res);
+                }
             });
 
-            bot.api.chat.postMessage({'channel' : 'general', 'text' : dayOfTheWeekResponses.statementResponse(), 'as_user' : true}, function (err, res) {
-                console.log(res);
-            });
         },
         start: false,
         timeZone: 'America/Chicago'
