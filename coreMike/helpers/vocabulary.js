42add2007353ee69f398333182ee4b39e955f955
Kevin Roberts <kroberts@mathworks.com>
2016-04-20 22:10:30 -0500

    update vocab

--- coreMike/helpers/vocabulary.js
+++ coreMike/helpers/vocabulary.js
@@ -7,9 +7,19 @@ var mikeDangs = [
     'goddamn'
 ];
 
+var sadMikes = [
+    ':thumbsdown::skin-tone-5:',
+    ':rage:',
+    ':sob:'
+];
+
 module.exports = {
     getMikeDang: function getMikeDang() {
         var index = Math.floor(Math.random() * mikeDangs.length);
         return mikeDangs[index];
+    },
+    getSadMikeReaction: function getMikeDang() {
+        var index = Math.floor(Math.random() * sadMikes.length);
+        return sadMikes[index];
     }
 }
--- coreMike/responses/dayOfTheWeek.js
+++ coreMike/responses/dayOfTheWeek.js
@@ -17,7 +17,7 @@ module.exports = {
         var day = getDayOfWeek();
         var msg = '';
         if (day == 1) {
-            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! :thumbsdown::skin-tone-5:";
+            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction();
         }
         if (day == 2) {
             msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
@@ -49,22 +49,22 @@ module.exports = {
 
         var msg = '';
         if (day == 1) {
-            msg = "DANG MONDAY DON'T TALK TO ME!! :thumbsdown::skin-tone-5:";
+            msg = vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction();
         }
         if (day == 2) {
-            msg = "HAPPY GOD DANG HUMPDAY'S EVE!! :fist::skin-tone-5:";
+            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
         }
         if (day == 3) {
-            msg = "HAPPY GOD DANG :camel: HUMPDAY :camel: !!!! :fist::skin-tone-5:";
+            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " :camel: HUMPDAY :camel: !!!! :fist::skin-tone-5:";
         }
         if (day == 4) {
-            msg = "HAPPY GOD DANG FRIDAY'S EVE!!!! :fist::skin-tone-5:";
+            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY'S EVE!!!! :fist::skin-tone-5:";
         }
         if (day == 5) {
-            msg = "HAPPY GOD DANG FRIDAY!!!! :fist::skin-tone-5:";
+            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY!!!! :fist::skin-tone-5:";
         }
         if (day > 5) {
-            msg = "HAPPY MOTHER FLIPPIN WEEKEND BRO!!!! :fist::skin-tone-5:";
+            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " WEEKEND BRO!!!! :fist::skin-tone-5:";
         }
 
         return msg;
