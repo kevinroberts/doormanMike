bee740914fda9055388a26d9fd99730dd0f58c57
Kevin Roberts <kroberts@mathworks.com>
2016-05-06 10:49:07 -0500

    cleanup

--- app.js
+++ app.js
@@ -10,39 +10,40 @@ var debugMode = false;
 
 // Keep bot from starting if a Slack token is missing
 if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+    console.log('Error: Specify token in environment');
+    process.exit(1);
 }
+
 if (!process.env.FIREBASEURI) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+    console.log('Error: Specify token in environment');
+    process.exit(1);
 }
+
 if (!process.env.NODE_ENV) {
   console.warn('Warning: Specify an environment mode');
 } else {
   var development = process.env.NODE_ENV !== 'production';
 
   if (development) {
-    debugMode = true;
+      debugMode = true;
   }
 }
 
 var firebaseStorage = require('botkit-storage-firebase')({firebase_uri: process.env.FIREBASEURI});
 
 var controller = Botkit.slackbot({
-  debug: debugMode,
-  storage: firebaseStorage
+    debug: debugMode,
+    storage: firebaseStorage
 });
 
 var bot = controller.spawn({
-  token: process.env.token
+    token: process.env.token
 }).startRTM();
 
 baseResponses(controller, null);
 
 // start scheduled mike messages
 scheduledResponses(bot);
-
 
 
 module.exports = bot;
--- coreMike/responses/conversations.js
+++ coreMike/responses/conversations.js
@@ -187,4 +187,4 @@ module.exports = {
 
     }
 
-}
+};
