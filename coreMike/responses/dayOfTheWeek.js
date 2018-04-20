27da8b022b289ddc7a20d43a48503327db2a9093
Kevin Roberts <kevin.roberts@icfolson.com>
2018-04-20 13:04:05 -0500

    add new monday responses

--- coreMike/responses/dayOfTheWeek.js
+++ coreMike/responses/dayOfTheWeek.js
@@ -1,5 +1,4 @@
-
-var dayOfWeek = require('day-of-week').get
+var dayOfWeek = require('day-of-week').get;
 var vocabulary = require('../helpers/vocabulary');
 var messageUtils = require('../helpers/messageUtils');
 var _ = require('lodash');
@@ -11,175 +10,179 @@ var mikeWednesdays = [
   'IT\'S WEDNESDAY MAH DUDES!! https://storage.googleapis.com/unleash-the-shart.appspot.com/wednesday.png'
 ];
 
-function getDefaultTz() {
-    if (timezoneEnv == null) {
-        return 'America/Chicago';
-    } else {
-        return timezoneEnv;
-    }
+var mikeMondays = [
+  "|MIKE_DANG| MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction(),
+  'IT\'S |MIKE_DANG| Monday!! :sick: https://storage.googleapis.com/unleash-the-shart.appspot.com/today_is_monday.jpg'
+];
+
+function getDefaultTz () {
+  if (timezoneEnv == null) {
+    return 'America/Chicago';
+  } else {
+    return timezoneEnv;
+  }
 }
 
-function getDayOfWeek(timezone) {
-    if (timezone == null) {
-        return dayOfWeek(new Date(), getDefaultTz());
-    } else {
-        return dayOfWeek(new Date(), timezone);
-    }
+function getDayOfWeek (timezone) {
+  if (timezone == null) {
+    return dayOfWeek(new Date(), getDefaultTz());
+  } else {
+    return dayOfWeek(new Date(), timezone);
+  }
 }
 
 module.exports = {
 
-    questionResponse: function (username, bot, message) {
-        var day = getDayOfWeek();
-        var msg = '';
-        if (day == 1) {
-            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction();
-        }
-        if (day == 2) {
-            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
-        }
-        if (day == 3) {
-          msg = _.sample(mikeWednesdays);
-          msg = msg.replace("|MIKE_DANG|", vocabulary.getMikeDang().toUpperCase());
-          if (bot != null && message != null) {
-              messageUtils.postReaction(bot, message, 'camel');
-          }
-        }
-        if (day == 4) {
-            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY'S EVE!!!! :fist::skin-tone-5:";
-        }
-        if (day == 5) {
-            msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY!!!! :fist::skin-tone-5:";
-            if (bot != null && message != null) {
-                messageUtils.postMikeFist(bot, message);
-            }
-        }
-        if (day > 5) {
-            msg = "IT'S THE " + vocabulary.getMikeDang().toUpperCase() + " WEEKEND BRO!!!! :fist::skin-tone-5:";
-        }
-        if (username != null) {
-            msg = username + ' ' + msg;
-        }
-        return msg;
-    },
+  questionResponse: function (username, bot, message) {
+    var day = getDayOfWeek();
+    var msg = '';
+    if (day == 1) {
+      msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction();
+    }
+    if (day == 2) {
+      msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
+    }
+    if (day == 3) {
+      msg = _.sample(mikeWednesdays);
+      msg = msg.replace("|MIKE_DANG|", vocabulary.getMikeDang().toUpperCase());
+      if (bot != null && message != null) {
+        messageUtils.postReaction(bot, message, 'camel');
+      }
+    }
+    if (day == 4) {
+      msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY'S EVE!!!! :fist::skin-tone-5:";
+    }
+    if (day == 5) {
+      msg = "it's " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY!!!! :fist::skin-tone-5:";
+      if (bot != null && message != null) {
+        messageUtils.postMikeFist(bot, message);
+      }
+    }
+    if (day > 5) {
+      msg = "IT'S THE " + vocabulary.getMikeDang().toUpperCase() + " WEEKEND BRO!!!! :fist::skin-tone-5:";
+    }
+    if (username != null) {
+      msg = username + ' ' + msg;
+    }
+    return msg;
+  },
 
-    statementResponse: function () {
+  statementResponse: function () {
 
-        var day = getDayOfWeek();
+    var day = getDayOfWeek();
 
-        var msg = '';
-        if (day == 1) {
-            msg = vocabulary.getMikeDang().toUpperCase() + " MONDAY DON'T TALK TO ME!! " + vocabulary.getSadMikeReaction();
-        }
-        if (day == 2) {
-            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
-        }
-        if (day == 3) {
-          msg = _.sample(mikeWednesdays);
-          msg = msg.replace("|MIKE_DANG|", vocabulary.getMikeDang().toUpperCase());
-        }
-        if (day == 4) {
-            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY'S EVE!!!! :fist::skin-tone-5:";
-        }
-        if (day == 5) {
-            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY!!!! :fist::skin-tone-5:";
-        }
-        if (day > 5) {
-            msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " WEEKEND BRO!!!! :fist::skin-tone-5:";
-        }
+    var msg = '';
+    if (day == 1) {
+      msg = _.sample(mikeMondays);
+      msg = msg.replace("|MIKE_DANG|", vocabulary.getMikeDang().toUpperCase());
+    }
+    if (day == 2) {
+      msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " HUMPDAY'S EVE!! :fist::skin-tone-5:";
+    }
+    if (day == 3) {
+      msg = _.sample(mikeWednesdays);
+      msg = msg.replace("|MIKE_DANG|", vocabulary.getMikeDang().toUpperCase());
+    }
+    if (day == 4) {
+      msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY'S EVE!!!! :fist::skin-tone-5:";
+    }
+    if (day == 5) {
+      msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " FRIDAY!!!! :fist::skin-tone-5:";
+    }
+    if (day > 5) {
+      msg = "HAPPY " + vocabulary.getMikeDang().toUpperCase() + " WEEKEND BRO!!!! :fist::skin-tone-5:";
+    }
 
-        return msg;
-    },
+    return msg;
+  },
 
+  favoriteDayResponse: function (user) {
+    var day = getDayOfWeek();
 
-    favoriteDayResponse: function (user) {
-        var day = getDayOfWeek();
+    var mikeResponses = [
+      '|USERNAME| IT\'S DEFINITELY NOT ' + vocabulary.getMikeDang().toUpperCase() + " MONDAY... I HATE MONDAY " + vocabulary.getSadMikeReaction(),
+      "MONDAY... JUST kidding I " + vocabulary.getMikeDang().toUpperCase() + " HATE MONDAY.",
+      vocabulary.getMikeDang().toUpperCase() + ' FRRIIIDAYYYYYYYYYY! :whiskey: ',
+      'the lord made friday and should have stopped there. TGIF is my favorite place on earth. Also I got shit to do this weekend. :fist::skin-tone-5: ',
+      '|USERNAME| IT IS ' + vocabulary.getMikeDang().toUpperCase() + ' FRIDAY :heart: OF COURSE! The welcome wagon to the weekend. '
+    ];
 
-        var mikeResponses = [
-            '|USERNAME| IT\'S DEFINITELY NOT ' + vocabulary.getMikeDang().toUpperCase() + " MONDAY... I HATE MONDAY " + vocabulary.getSadMikeReaction(),
-            "MONDAY... JUST kidding I " + vocabulary.getMikeDang().toUpperCase() + " HATE MONDAY.",
-            vocabulary.getMikeDang().toUpperCase() + ' FRRIIIDAYYYYYYYYYY! :whiskey: ',
-            'the lord made friday and should have stopped there. TGIF is my favorite place on earth. Also I got shit to do this weekend. :fist::skin-tone-5: ',
-            '|USERNAME| IT IS ' + vocabulary.getMikeDang().toUpperCase() + ' FRIDAY :heart: OF COURSE! The welcome wagon to the weekend. '
-        ];
-        
-        var msg = _.sample(mikeResponses).replace("|USERNAME|", user);
+    var msg = _.sample(mikeResponses).replace("|USERNAME|", user);
 
-        if (day == 1) {
-            var mikeResponses2 = [
-                "I haven't had this bad of a Monday since last Monday...",
-                "ugh it actually is monday *shudders*",
-                "God gave us Mondays to punish us for the things we did over the weekend.",
-                "...good thing i got some :whiskey: under this desk"
-            ];
+    if (day == 1) {
+      var mikeResponses2 = [
+        "I haven't had this bad of a Monday since last Monday...",
+        "ugh it actually is monday *shudders*",
+        "God gave us Mondays to punish us for the things we did over the weekend.",
+        "...good thing i got some :whiskey: under this desk"
+      ];
 
-            msg += "\n" + _.sample(mikeResponses2);
+      msg += "\n" + _.sample(mikeResponses2);
 
-        }
+    }
 
-        if (day == 5) {
-            msg = "ITS DEFINITELY " + vocabulary.getMikeDang().toUpperCase() + " TODAY - CUZ ITS FRIDAY! TGIF!"
-        }
+    if (day == 5) {
+      msg = "ITS DEFINITELY " + vocabulary.getMikeDang().toUpperCase() + " TODAY - CUZ ITS FRIDAY! TGIF!"
+    }
 
-        if (day > 5) {
-            msg += "\nand its the weekend soo stop asking me silly questions."
-        }
+    if (day > 5) {
+      msg += "\nand its the weekend soo stop asking me silly questions."
+    }
 
+    return msg;
+  },
 
-        return msg;
-    },
+  getMikeMorninTimeSensitive: function (user) {
+    var mikeDang = vocabulary.getMikeDang();
+    var mikeMornin = vocabulary.getMikeMornin();
 
-    getMikeMorninTimeSensitive: function (user) {
-        var mikeDang = vocabulary.getMikeDang();
-        var mikeMornin = vocabulary.getMikeMornin();
+    var myDate = new Date();
+    // only trigger if hour is before noon
+    var dayResponse = this.statementResponse();
+    if (myDate.getHours() < 12) {
+      if (user) {
+        return user + " " + mikeMornin + "\n" + dayResponse;
+      } else {
+        return mikeMornin + "\n" + dayResponse;
+      }
+    }
+    /* Hour is from noon to 5pm (actually to 5:59 pm) */
+    else if (myDate.getHours() >= 12 && myDate.getHours() <= 17) {
+      if (user) {
+        return user + " get to that sack-room it's da afternoon yo!";
+      } else {
+        return "get to that sack-room it's da afternoon yo!";
+      }
+    }
+    /* the hour is after 5pm, so it is between 6pm and midnight */
+    else if (myDate.getHours() > 17 && myDate.getHours() <= 24) {
+      if (user) {
+        return user + " I'M " + mikeDang.toUpperCase() + " :sleeping: NOW GO AWAY DOORMAN MIKE NEEDS HIS BEAUTY SLEEP";
+      } else {
+        return "I am " + mikeDang + " zZz :sleeping: ";
+      }
+    }
+  },
 
-        var myDate = new Date();
-        // only trigger if hour is before noon
-        var dayResponse = this.statementResponse();
-        if (myDate.getHours() < 12) {
-            if (user) {
-                return user + " " + mikeMornin + "\n" + dayResponse;
-            } else {
-                return mikeMornin + "\n" + dayResponse;
-            }
-        }
-        /* Hour is from noon to 5pm (actually to 5:59 pm) */
-        else if (myDate.getHours() >= 12 && myDate.getHours() <= 17) {
-            if (user) {
-                return user + " get to that sack-room it's da afternoon yo!";
-            } else {
-                return "get to that sack-room it's da afternoon yo!";
-            }
-        }
-        /* the hour is after 5pm, so it is between 6pm and midnight */
-        else if (myDate.getHours() > 17 && myDate.getHours() <= 24) {
-            if (user) {
-                return user + " I'M " + mikeDang.toUpperCase() + " :sleeping: NOW GO AWAY DOORMAN MIKE NEEDS HIS BEAUTY SLEEP";
-            } else {
-                return "I am " + mikeDang + " zZz :sleeping: ";
-            }
-        }
-    },
+  getMikeMondayResponse: function (user, bot, message) {
+    var mikeResponses = [
+      '|USERNAME| eew :scream: ' + vocabulary.getMikeDang().toUpperCase() + " MONDAY... I HATE MONDAY ",
+      "MONDAY i dont even like saying the " + vocabulary.getMikeDang() + " word",
+      "|USERNAME|, God gave us Mondays to punish us for the things we did over the weekend."
+    ];
 
-    getMikeMondayResponse: function (user, bot, message) {
-        var mikeResponses = [
-            '|USERNAME| eew :scream: ' + vocabulary.getMikeDang().toUpperCase() + " MONDAY... I HATE MONDAY ",
-            "MONDAY i dont even like saying the " + vocabulary.getMikeDang() + " word",
-            "|USERNAME|, God gave us Mondays to punish us for the things we did over the weekend."
-        ];
+    var mikeReactions = [
+      'omg',
+      "rage",
+      "wtf"
+    ];
 
-        var mikeReactions = [
-            'omg',
-            "rage",
-            "wtf"
-        ];
+    messageUtils.postReaction(bot, message, _.sample(mikeReactions));
 
-        messageUtils.postReaction(bot, message, _.sample(mikeReactions));
-        
-        return _.sample(mikeResponses).replace("|USERNAME|", user);
-    },
+    return _.sample(mikeResponses).replace("|USERNAME|", user);
+  },
 
-    getMikeFridayResponse: function (user, bot, message) {
+  getMikeFridayResponse: function (user, bot, message) {
     var mikeResponses = [
       '|USERNAME| fucck yea' + vocabulary.getMikeDang().toUpperCase() + " FRIDAY... I LOVE IT ",
       "FRIDAY i could marry the " + vocabulary.getMikeDang() + " word",
@@ -196,36 +199,36 @@ module.exports = {
 
     return _.sample(mikeResponses).replace("|USERNAME|", user);
   },
-    getMikeWeekendResponse: function () {
-        var day = getDayOfWeek();
+  getMikeWeekendResponse: function () {
+    var day = getDayOfWeek();
 
-        if (day > 5) {
-            var mikeResponses = [
-                "Two days is not enough time for the Weekend.",
-                "Nothing in the world is more expensive than a woman who’s free for the weekend. :dancer: :fist::skin-tone-5:",
-                "It's mah " + vocabulary.getMikeDang() + " weekend leave me alone."
-            ];
-            return _.sample(mikeResponses);
+    if (day > 5) {
+      var mikeResponses = [
+        "Two days is not enough time for the Weekend.",
+        "Nothing in the world is more expensive than a woman who’s free for the weekend. :dancer: :fist::skin-tone-5:",
+        "It's mah " + vocabulary.getMikeDang() + " weekend leave me alone."
+      ];
+      return _.sample(mikeResponses);
 
-        } else {
-            var mikeResponses = [
-                "Its not the weekend yet, ask me later",
-                "Why wait for the " + vocabulary.getMikeDang() + " weekend to have fun? :whiskey:",
-                "Yeah it's not the weekend bro. better days are coming."
-            ];
+    } else {
+      var mikeResponses = [
+        "Its not the weekend yet, ask me later",
+        "Why wait for the " + vocabulary.getMikeDang() + " weekend to have fun? :whiskey:",
+        "Yeah it's not the weekend bro. better days are coming."
+      ];
 
-            return _.sample(mikeResponses);
+      return _.sample(mikeResponses);
 
-        }
-
-    },
-
-    getDayOfTheWeek: function (timezone) {
-        if (timezone == null) {
-            return dayOfWeek(new Date(), getDefaultTz());
-        } else {
-            return dayOfWeek(new Date(), timezone);
-        }
     }
+
+  },
+
+  getDayOfTheWeek: function (timezone) {
+    if (timezone == null) {
+      return dayOfWeek(new Date(), getDefaultTz());
+    } else {
+      return dayOfWeek(new Date(), timezone);
+    }
+  }
 
 };
